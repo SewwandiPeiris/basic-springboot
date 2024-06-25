d8aeeedee2312d96bcb210d23eb3fc17d5e3e978
SewwandiPeiris <peirissewwandi@gmail.com>
2024-06-25 23:32:44 +0530

    Builder Without Project Lombok

--- src/main/java/edu/icet/Main.java
+++ src/main/java/edu/icet/Main.java
@@ -6,6 +6,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Student st = new Student();
+
+        st.setFirstName("Kasun").setLastName("Anuruddha").setContactNumber("0987654321"); //builder
+
         st.setFirstName("Kasun");
         st.setLastName("Anuruddha");
         st.setContactNumber("0987654321");
